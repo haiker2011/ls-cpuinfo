4b3119cb61f0c079cb2d510e07d45029abbc1596
孙海洲 <haizhou.uestc2011@gmail.com>
2018-08-30 15:30:45 +0800

    Create cpu_info.sh

---
+++ cpu_info.sh
@@ -0,0 +1,11 @@
+# 查看CPU型号
+cat /proc/cpuinfo | grep name | cut -f2 -d: | uniq -c
+
+# 查看物理CPU个数
+cat /proc/cpuinfo| grep "physical id"| sort| uniq| wc -l
+
+# 查看每个物理CPU中core的个数(即核数)
+cat /proc/cpuinfo| grep "cpu cores"| uniq
+
+# 查看逻辑CPU的个数
+cat /proc/cpuinfo| grep "processor"| wc -l
